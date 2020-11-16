512c6b36d854b9ce999f65837da32bc2c510a9a3
Marcelo Glacial <marceloglacial@marcelos-mbp.lan>
2020-11-16 07:47:30 -0500

    update commands

--- macos-fresh-install.sh
+++ macos-fresh-install.sh
@@ -7,10 +7,10 @@ echo "Updating gems..."
 gem update
 
 echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Installing brew cask..."
-brew tap caskroom/cask
+brew tap homebrew/cask
 
 
 # Dev Tools
